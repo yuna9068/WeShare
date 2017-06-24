d2faeab162676c9f621779bb2cc9ed18815316a0
yuna9068 <yuna9068@gmail.com>
2017-06-25 04:30:24 +0800

    login.html 更新
    login.html
    - 加<form>

--- dist/js/login.js
+++ dist/js/login.js
@@ -1,6 +1,6 @@
 $(function(){
 	console.log("loginOk = " + loginOk, "indname = " + indname, "indid = " + indid);
-	if (indname == ""){
+	if (indname == "" || indname == "${LoginOK.indname}"){
 		$(".afterLogin").css("display", "none");
 	} else {
 		$(".beforeLogin").css("display", "none");
