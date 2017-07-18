ce853a7caa70b3d72eed33fc04559ababf1ed675
TpWILovePanda <zxtw17985321@gmail.com>
2017-07-18 11:48:28 +0800

    js、css及web 更新
    js
    - feedback.js
    --- 評價分數
    
    css
    - feedback.css
    - org.css
    
    web
    - feedback.html
    --- 評價分數

--- dist/js/feedback.js
+++ dist/js/feedback.js
@@ -5,24 +5,35 @@
 // 4.0分 4星 0半星 1空星
 // 4.2分 4星 1半星 0空星
 
-function starSetArry (starScore) {
-    var starTotal = 5;
-    var starArry = [];
-    var starString = "";
-    // 0: 空星, 1: 半星, 2: 實星
-    for (var i = 0; i < starTotal; i++) {
-        if (starScore >= 1) {
-            starScore = starScore - 1;
-            starArry[i] = 2;
-            starString += `<span><i class="fa fa-star" aria-hidden="true"></i></span>`;
-        } else if ( 0 < starScore && starScore < 1) {
-            starScore = starScore - starScore;
-            starArry[i] = 1;
-            starString += `<span><i class="fa fa-star-half-o" aria-hidden="true"></i></span>`;
-        } else if (starScore == 0) {
-            starArry[i] = 0;
-            starString += `<span><i class="fa fa-star-o" aria-hidden="true"></i></span>`;
-        }
-    }
-    console.log(starArry);
+function starSetArry(starScore) {
+	var starTotal = 5;
+	var starArry = [];
+	var starString = "";
+	// 0: 空星, 1: 半星, 2: 實星
+	for (var i = 0; i < starTotal; i++) {
+		if (starScore >= 1) {
+			starScore = starScore - 1;
+			starArry[i] = 2;
+			starString += `<span><i class="fa fa-star" aria-hidden="true"></i></span>`;
+		} else if (0 < starScore && starScore < 1) {
+			starScore = starScore - starScore;
+			starArry[i] = 1;
+			starString += `<span><i class="fa fa-star-half-o" aria-hidden="true"></i></span>`;
+		} else if (starScore == 0) {
+			starArry[i] = 0;
+			starString += `<span><i class="fa fa-star-o" aria-hidden="true"></i></span>`;
+		}
+	}
+	console.log(starArry);
 }
+
+$(function () {
+	var $ppc = $('.progress-pie-chart'),
+		percent = parseInt($ppc.data('percent')),
+		deg = 360 * percent / 100;
+	if (percent > 50) {
+		$ppc.addClass('gt-50');
+	}
+	$('.ppc-progress-fill').css('transform', 'rotate(' + deg + 'deg)');
+	$('.ppc-percents span').html(percent + '%');
+});
